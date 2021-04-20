b43fb8f6fcc3ae1a36db2890c2b5acbf8ab4e1a1
Matthew Johnson <43484727+mjohnson245@users.noreply.github.com>
2021-04-20 17:26:14 -0400

    Added step, getNeighbors, inc1, and inc23 functions

--- gamescript.js
+++ gamescript.js
@@ -1,3 +1,4 @@
+const intervalID = setInterval(step, 500);
 
 //draws grid with selected grid size
 function drawGrid(){
@@ -36,7 +37,7 @@ function createGrid(rows, cols, callback){
     return grid;
 }
 
-var cells;
+let cells;
 function createCells(){
 	var gridSize = document.getElementById("grid_size").value;
 	//console.log(gridSize);
@@ -58,23 +59,42 @@ function randomCells(){
 function drawCells() {
 
 }
-
+//grid size may need adjustment
+//drawCells needs to be added to see results
 function step() {
-
+	var gridSize = document.getElementById('grid_size').value;
+	//console.log(gridSize);
+	let newCells = createCells();
+		for (let y = 0; y < gridSize*gridSize; y++) {
+			for (let x = 0; x < gridSize*gridSize; x++) {
+				const neighbors = getNeighbors(x,y);
+				//Rule 1 and 2 applied
+				if (cells[x][y] && neighbors >= 2 && neighbors <= 3) {
+					newCells[x][y] = true;
+					//Rule 3 and 4  applied
+				} else if(!cells[x][y] && neighbors === 3) {
+					newCells[x][y] = true;
+				}
+			}
+		}
+		cells = newCells;
+		//drawCells();
 }
-
-/*function getNeighbors(x, y){
+//grid size may need adjustment
+//
+function getNeighbors(x, y){
+	var gs = document.getElementById('grid_size').value;
 	let count = 0;
 	for (let i = -1; i < 2; i++) {
 		for (let j = -1; j < 2; j++) {
 			if(i === 0 && j === 0) {
 				continue;
 			}
-			if(x+i < 0 || x+i > resolution - 1){
+			if(x+i < 0 || x+i > gs*gs - 1){
 				continue;
 			}
 
-			if(y+j < 0 || y+j > resolution - 1){
+			if(y+j < 0 || y+j > gs*gs - 1){
 				continue;
 			}
 			if (cells[x+i][y+j]) {
@@ -82,12 +102,19 @@ function step() {
 			}
 		}
 	}
+	console.log(count);
 	return count;
-}*/
+}
 
 function startGame(){
 	drawGrid();
-	createCells();
+	cells = createCells();
+	console.log('cells' + cells);
+	step();
+}
+
+function stopGame() {
+	clearInterval(intervalID);
 }
 
 //resets game
@@ -95,3 +122,14 @@ function resetGame(){
 	 document.getElementById("game_canvas").innerHTML = "";
 }
 
+function inc1(){
+	step();
+}
+
+function inc23(){
+	let a = 1;
+	while(a < 24) {
+		step();
+		a++;
+	}
+}
